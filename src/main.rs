77294e85822512e6c175b32c9d09e5c46d140b8d
Anatolii <chemyl.inc@gmail.com>
2024-11-05 10:55:38 +0600

    add iterator

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use mini_grep::Config;
 use std::{env, process};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
 
     if let Err(e) = mini_grep::run(config) {
         eprintln!("Application error: {e}");
